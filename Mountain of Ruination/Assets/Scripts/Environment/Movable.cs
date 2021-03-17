e4ba5c5a9043326f8b9c223ce5caec8385d1cd33
Timofey <timohatimafeikq@gmail.com>
2021-03-17 14:55:02 +0300

    Add interact buttom

---
+++ Mountain of Ruination/Assets/Scripts/Environment/Movable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Environment
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Movable : MonoBehaviour
+    {
+        #region Fields & properties
+
+        private Rigidbody2D _body;
+
+        #endregion
+
+        #region Unity calls
+
+        private void Start()
+        {
+            _body = GetComponent<Rigidbody2D>();
+        }
+
+        #endregion
+        
+    }
+}
--- Mountain of Ruination/Assets/Scripts/Environment/MovableController.cs
+++ Mountain of Ruination/Assets/Scripts/Environment/MovableController.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Interface.World;
+using UnityEngine;
 using Util;
 
 namespace Environment
@@ -7,43 +11,96 @@ namespace Environment
     {
         #region Fields and properties
 
-        #region Unity assigns
+        public InteractButton button;
         
-        public GameObject playerInteract;
-        public Rigidbody2D body;
-
-        #endregion
-        
-        private Vector3 _lastPosition;
+        private List<Movable> _movables;
+        private List<DistanceSortable<Movable>> _sortables;
+        private Movable _currentMovable;
+        private bool _isCurrentMovableSet;
 
         #endregion
 
         #region Unity calls
 
-        private void OnTriggerStay2D(Collider2D other)
+        private void Start()
         {
-            if (other.gameObject != playerInteract)
-            {
-                _lastPosition = playerInteract.transform.position;
-                return;
-            }
-            
-            if (InputUtil.GetInteract())
-            {
-                var diff =  playerInteract.transform.position - _lastPosition;
-                body.MovePosition(transform.position + diff);
-            }
-
-            _lastPosition = playerInteract.transform.position;
-
+            _movables = new List<Movable>();
+            _sortables = new List<DistanceSortable<Movable>>();
         }
 
-        private void OnTriggerExit(Collider other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            
+            if (!other.CompareTag(Tag.Movable)) return;
+            var movable = other.GetComponent<Movable>();
+            if (movable != null) _movables.Add(movable);
+        }
+
+        private void FixedUpdate()
+        {
+            if (_movables.Count == 0)
+            {
+                _isCurrentMovableSet = false;
+                return;
+            }
+
+            _sortables.Clear();
+            foreach (var movable in _movables)
+            {
+                var distance = movable.transform.position - transform.position;
+                _sortables.Add(new DistanceSortable<Movable>(movable, distance.magnitude));
+            }
+            _sortables.Sort();
+            _currentMovable = _sortables[0].Item;
+            _isCurrentMovableSet = true;
+        }
+
+        private void Update()
+        {
+            if (!_isCurrentMovableSet)
+            {
+                SwitchButton(false);
+                return;
+            }
+
+            SwitchButton(true);
+            button.transform.position = _currentMovable.transform.position;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (!other.CompareTag(Tag.Movable)) return;
+            var movable = other.GetComponent<Movable>();
+            if (movable != null) _movables.Remove(movable);
         }
 
         #endregion
-        
+
+        #region Support Methods
+
+        private void SwitchButton(bool enable)
+        {
+            button.Sprite.enabled = enable;
+        }
+
+        #endregion
+    }
+
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+    public struct DistanceSortable<T> : IComparable<DistanceSortable<T>>
+    {
+        public T Item { get; set; }
+        public float Distance { get; set; }
+ 
+        public DistanceSortable(T item, float distance)
+        {
+            Item = item;
+            Distance = distance;
+        }
+
+        public int CompareTo(DistanceSortable<T> other)
+        {
+            return Distance.CompareTo(other.Distance);
+        }
     }
 }
