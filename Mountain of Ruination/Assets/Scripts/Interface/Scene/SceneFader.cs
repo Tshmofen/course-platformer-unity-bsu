66afb2e98fb4d826871d87ca073b8c6398cdb4f5
Timofey <timohatimafeikq@gmail.com>
2021-03-31 14:20:03 +0300

    Add scene fader

---
+++ Mountain of Ruination/Assets/Scripts/Interface/Scene/SceneFader.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Interface.Scene
+{
+    public class SceneFader : MonoBehaviour
+    {
+        #region Fields & properties
+
+        [Header("Visuals")] 
+        public Image background;
+        public float fadeTime = 0.7f;
+        public float waitTime = 0.3f;
+
+        private float _currentWait;
+        private float _currentFade;
+
+        #endregion
+
+        #region Unity calls
+
+        private void Update()
+        {
+            if (_currentFade >= fadeTime)
+            {
+                Destroy(background.gameObject);
+                Destroy(this);
+            }
+
+            if (_currentWait < waitTime) 
+                _currentWait += Time.deltaTime;
+
+            if (_currentWait >= waitTime)
+            {
+                _currentFade += Time.deltaTime;
+                if (_currentFade > fadeTime) _currentFade = fadeTime;
+                
+                var color = background.color;
+                color.a = 1 - _currentFade / fadeTime;
+                background.color = color;
+            }
+        }
+
+        #endregion
+    }
+}
