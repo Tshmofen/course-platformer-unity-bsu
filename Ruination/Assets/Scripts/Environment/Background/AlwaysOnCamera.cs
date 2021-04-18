3b508fe7318688e91b19d87d7b7a5f413f8ae9a7
Timofey <timohatimafeikq@gmail.com>
2021-04-18 19:06:17 +0300

    Add on screen relocating script

---
+++ Ruination/Assets/Scripts/Environment/Background/AlwaysOnCamera.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Environment.Background
+{
+    public class AlwaysOnCamera : MonoBehaviour
+    {
+        private Camera _camera;
+
+        private Renderer _rendererPaired;
+        private Renderer _renderer;
+
+        public GameObject pairedObject;
+        
+        private void Start()
+        {
+            _camera = Camera.main;
+            _renderer = GetComponent<Renderer>();
+            _rendererPaired = pairedObject.GetComponent<Renderer>();
+        }
+        
+        private void OnBecameInvisible()
+        {
+            if (_camera == null) return;
+
+            var transformObject = transform;
+            var transformCamera = _camera.transform;
+            var positionObject = transformObject.position;
+            var positionCamera = transformCamera.position;
+            
+            var objectHalfWidth = _renderer.bounds.extents.x;
+            var pairedHalfWidth = _rendererPaired.bounds.extents.x;
+            var cameraHalfWidth = _camera.orthographicSize * _camera.aspect;
+
+            var objectRightEdgeInView =
+                positionObject.x + objectHalfWidth > positionCamera.x - cameraHalfWidth &&
+                positionObject.x + objectHalfWidth < positionCamera.x + cameraHalfWidth;
+            var objectLeftEdgeInView =
+                positionObject.x - objectHalfWidth > positionCamera.x - cameraHalfWidth &&
+                positionObject.x - objectHalfWidth < positionCamera.x + cameraHalfWidth;
+            if (objectRightEdgeInView || objectLeftEdgeInView ) 
+                return;
+
+            // if object right to camera add minus
+            var minus = (positionObject.x - positionCamera.x > 0) ? -1 : 1;
+            var newPosition = pairedObject.transform.position;
+            newPosition.x += minus * pairedHalfWidth * 0.99f;
+            newPosition.x += minus * objectHalfWidth;
+            transformObject.position = newPosition;
+        }
+    }
+}
--- Ruination/Assets/Scripts/Environment/Background/CloudController.cs
+++ Ruination/Assets/Scripts/Environment/Background/CloudController.cs
@@ -4,41 +4,34 @@ namespace Environment.Background
 {
     public class CloudController : MonoBehaviour
     {
-        #region Fields & properties
-
         private Camera _camera;
         
         public float speedX = 0.1f;
-
-        #endregion
-
-        #region Unity calls
 
         private void Start()
         {
             _camera = Camera.main;
         }
-
+        
         private void Update()
         {
             transform.position += new Vector3(speedX * Time.deltaTime, 0);
         }
-
+        
         private void OnBecameInvisible()
         {
             if (_camera == null) return;
             
-            var transformCloud = transform;
             var transformCamera = _camera.transform;
+            var positionObject = transform.position;
             var positionCamera = transformCamera.position;
-            var positionCloud = transformCloud.position;
-            
-            var minus = (positionCloud.x - positionCamera.x > 0) ? -1 : 1;
-            var newX = positionCamera.x + minus * 1.2f * _camera.orthographicSize * _camera.aspect;
-            transformCloud.position = new Vector3(newX, positionCloud.y);
-        }
 
-        #endregion
-        
+            // if object right to camera add minus
+            var minus = (positionObject.x - positionCamera.x > 0) ? -1 : 1;
+            var newX = positionCamera.x;
+            newX += minus * _camera.orthographicSize * _camera.aspect;
+            newX += minus * GetComponent<Renderer>().bounds.extents.x;
+            transform.position = new Vector3(newX, positionObject.y);
+        }
     }
 }
