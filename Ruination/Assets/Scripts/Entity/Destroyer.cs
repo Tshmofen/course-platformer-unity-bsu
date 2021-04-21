db4e1702f6d0ba2f651f37d3d89029453314ccd9
Timofey <timohatimafeikq@gmail.com>
2021-04-21 14:33:42 +0300

    Add basic water representation

--- Ruination/Assets/Scripts/Entity/Destroyer.cs
+++ Ruination/Assets/Scripts/Entity/Destroyer.cs
@@ -12,17 +12,20 @@ namespace Entity
         public Material dyingMaterial;
         public float noiseScale = 20;
         public bool disableOnDeath;
+        public Color edgeColor;
+        public float colorIntensity = 1;
 
         #endregion
 
         private Material _currentMaterial;
         
-        private static readonly int Fade = Shader.PropertyToID("_Fade");
-        private static readonly int Scale = Shader.PropertyToID("_Scale");
+        private static readonly int HashFade = Shader.PropertyToID("_Fade");
+        private static readonly int HashScale = Shader.PropertyToID("_Scale");
+        private static readonly int HashEdgeColor = Shader.PropertyToID("_EdgeColor");
 
         public float CurrentFade
         {
-            set => _currentMaterial.SetFloat(Fade, value);
+            set => _currentMaterial.SetFloat(HashFade, value);
         }
 
         #endregion
@@ -32,7 +35,12 @@ namespace Entity
         public void EnableDeathMaterial()
         {
             _currentMaterial = Instantiate(dyingMaterial);
-            _currentMaterial.SetFloat(Scale, noiseScale);
+            _currentMaterial.SetFloat(HashScale, noiseScale);
+            var newColor = new Color(edgeColor.r * colorIntensity, 
+                edgeColor.g * colorIntensity, 
+                edgeColor.b * colorIntensity,
+                edgeColor.a);
+            _currentMaterial.SetColor(HashEdgeColor, newColor);
             foreach (var sprite in GetComponentsInChildren<SpriteRenderer>())
             {
                 sprite.material = _currentMaterial;
