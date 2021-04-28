1b20dfa4c653e2e4a070d98dfadea70815c690a2
Timofey <timohatimafeikq@gmail.com>
2021-04-28 16:59:28 +0300

    Add hiding areas

---
+++ Ruination/Assets/Scripts/Environment/Interactive/Hider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Environment.Interactive
+{
+    [RequireComponent(typeof(Tilemap))]
+    public class Hider : MonoBehaviour
+    {
+        private Tilemap _tiles;
+
+        public float fadeSpeed = 0.3f;
+
+        private void Start()
+        {
+            _tiles = GetComponent<Tilemap>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            StartCoroutine(FadeColor());
+            GetComponent<Collider2D>().enabled = false;
+        }
+
+        private IEnumerator FadeColor()
+        {
+            while (_tiles.color.a > 0)
+            {
+                var currentColor = _tiles.color;
+                currentColor.a -= fadeSpeed * Time.deltaTime;
+                if (currentColor.a < 0) currentColor.a = 0;
+                _tiles.color = currentColor;
+                yield return null;
+            }
+        }
+    }
+}
