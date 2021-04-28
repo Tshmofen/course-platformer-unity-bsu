2257168bab59b07cdbdc7f7848e8c120bbc86c52
Timofey <timohatimafeikq@gmail.com>
2021-04-28 13:19:47 +0300

    Fix physic activator, add death area

---
+++ Ruination/Assets/Scripts/Environment/Interactive/DeathArea.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Damage;
+using Entity;
+using UnityEngine;
+
+namespace Environment.Interactive
+{
+    [RequireComponent(typeof(DamageStats))]
+    public class DeathArea : MonoBehaviour
+    {
+        private DamageStats _stats;
+
+        public DamageType damageType = DamageType.HeavyDamage;
+
+        private void Start()
+        {
+            _stats = GetComponent<DamageStats>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var manager = other.GetComponent<EntityManager>();
+            if (manager != null)
+                manager.health.ReceiveDamage(_stats, damageType);
+        }
+    }
+}
